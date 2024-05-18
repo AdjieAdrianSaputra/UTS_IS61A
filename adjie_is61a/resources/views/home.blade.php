4f9d536a7bf28b535d94543e11ab1c8eb32632f7
AdjieAdrianSaputra <adjieadrian12@gmail.com>
2024-05-19 00:21:29 +0700

    Update halaman dashboard

--- adjie_is61a/resources/views/home.blade.php
+++ adjie_is61a/resources/views/home.blade.php
@@ -1,3 +1,11 @@
 @extends('layouts.master')
 @section('title','Dashboard')
 @section('heading','Halaman Dashboard')
+
+@section('content')
+        <div class="card-body">
+        Halo {{ Auth::user()->name }}, <br> Dashboard Coming Soon! Harap Sabarr
+        </div>
+        <!-- /.card-body -->
+
+@endsection
